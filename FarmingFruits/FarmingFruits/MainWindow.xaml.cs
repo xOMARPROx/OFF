613745db314436012f55f78c112ee0b20d49b932
sd <sd@fathallaiats.edu>
2024-11-12 11:05:56 +0200

    FarmingFruits

---
+++ FarmingFruits/FarmingFruits/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FarmingFruits
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public FarmingFruitsEntities2 Farming = new FarmingFruitsEntities2();
+        public UserDeatil deatil;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void UserTextMethod(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void PassTextMethod(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void ConfirmBtnMethod(object sender, RoutedEventArgs e)
+        {
+            using (var Check = new FarmingFruitsEntities2())
+            {
+                var user = Check.UserDeatils.SingleOrDefault(u => u.Usr_Name == UsrText.Text && u.Usr_Pass == PassText.Password);
+                if (user != deatil)
+                {
+                    MessageBox.Show("done");
+                    Window1 w = new Window1();
+                    w.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Error"); ;
+                }
+            }
+        }
+    }
+}
---
+++ FarmingFruits/FarmingFruits/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmingFruits
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        FarmingFruitsEntities2 fe = new FarmingFruitsEntities2();
+        public Fruit f = new Fruit();
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public void load()
+        {
+            ListSee.ItemsSource = fe.Fruits.ToList();
+        }
+
+        private void ListSee_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            f = (Fruit)ListSee.SelectedItem;
+        }
+    }
+}
